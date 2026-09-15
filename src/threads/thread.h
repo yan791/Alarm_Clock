e7c1a4a20ee20da56a665c4a651178132218dc88
Yan Nunes <yrn@cesar.school>
2026-09-15 09:39:01 -0300

    adicionando tick de despertar à estrutura de thread

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -87,7 +87,8 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
+    int priority;  
+    int64_t wakeup_tick;                     /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
