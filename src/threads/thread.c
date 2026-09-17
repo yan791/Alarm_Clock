450ecc570721eaaf938ddec2754a42f688883130
Yan Nunes <yrn@cesar.school>
2026-09-17 09:19:23 -0300

    inicializa lista de threads adromecidas

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -91,8 +91,8 @@ thread_init (void)
 {
   ASSERT (intr_get_level () == INTR_OFF);
 
-  lock_init (&tid_lock);
   list_init (&ready_list);
+  list_init (&sleeping_list);
   list_init (&all_list);
 
   /* Set up a thread structure for the running thread. */
